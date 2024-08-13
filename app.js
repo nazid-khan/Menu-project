3c40a1b77b83b5365fff9370d4e31a0912bf9fd1
nazid <nazidk773@gmail.com>
2024-08-13 12:16:48 +0530

    select menu items

--- app.js
+++ app.js
@@ -78,18 +78,18 @@ const sectionCenter = document.querySelector(".section-center");
 window.addEventListener("DOMContentLoaded",function(){
   let displayMenu = menu.map(function(item){
   console.log(item); 
-    return '<article class="menu-item">
-              <img src=${item.img} alt=${item.title} class="photo" />
-              <div class="item-info">
-               <header>
-                 <h4>${item.title}</h4>
-                 <h4 class="price">${item.price}</h4>
-               </header>
-               <p class="item-text">
-                 ${item.desc}
-               </p>
-              </div>
-           </article>';
+    // return '<article class="menu-item">
+    //           <img src=${item.img} alt=${item.title} class="photo" />
+    //           <div class="item-info">
+    //            <header>
+    //              <h4>${item.title}</h4>
+    //              <h4 class="price">${item.price}</h4>
+    //            </header>
+    //            <p class="item-text">
+    //              ${item.desc}
+    //            </p>
+    //           </div>
+    //         </article>';
 
   });
 

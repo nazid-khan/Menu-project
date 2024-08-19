cb5192bddeb9e92ef801100db77b3626a4509e28
nazid <nazidk773@gmail.com>
2024-08-19 11:33:44 +0530

    display all items

--- app.js
+++ app.js
@@ -77,19 +77,19 @@ const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener("DOMContentLoaded",function(){
   let displayMenu = menu.map(function(item){
-  // console.log(item); 
-    return '<article class="menu-item">'
-              '<img src=${item.img} alt=${item.title} class="photo" />'
-              '<div class="item-info">'
-               '<header>'
-                ' <h4>${item.title}</h4>'
-                ' <h4 class="price">${item.price}</h4>'
-               '</header>'
-               '<p class="item-text">'
-                ' ${item.desc}'
-               '</p>'
-              '</div>'
-            '</article>';
+  console.log(item); 
+    return `<article class="menu-item">
+              <img src=${item.img} alt=${item.title} class="photo" />
+              <div class="item-info">
+               <header>
+                 <h4>${item.title}</h4>
+                 <h4 class="price">${item.price}</h4>
+               </header>
+               <p class="item-text">
+                 ${item.desc}
+               </p>
+              </div>
+            </article>`;
 
   });
 
